809d56443a18d7dade549b5a1047ca1952a4a8a8
Shaun.xiongzheng <shaun_zheng@163.com>
2022-03-05 00:09:53 -0500

    updated

--- exercise/school-explorer-redux/index.js
+++ exercise/school-explorer-redux/index.js
@@ -17,13 +17,13 @@ const schoolList = document.querySelector('#school-list');
 const gradeLevelSelect = document.querySelector('#grade-level-select');
 const zipCodeSelect = document.querySelector('#zip-code-select');
 
-var redIcon = new L.Icon({
+const redIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41]
+  shadowSize: [41, 41],
 });
 
 /* ====================
@@ -32,20 +32,20 @@ var redIcon = new L.Icon({
 
 ==================== */
 
-let HighlightSchoolFunction = function (childArray) {
+let HighlightSchoolFunction = (childArray) => {
   let HighLightSchoolInfo = [
     `Asian American %: ${childArray.AsianPCT}`,
     `Black African American %: ${childArray.BlackAfricanAmericanPCT}`,
     `Hispantic American %: ${childArray.HispanicPCT}`,
     `White American %: ${childArray.WhitePCT}`];
-  let el4 = htmlToElement(`<strong><li style="color:#CA263D;">Grades: ${childArray.GradeLevel}</li></strong>`);
-  HighLightSchool.appendChild(htmlToElement(`<li style="color:#CA263D;"> <br></li>`)).appendChild(el4);
-  HighLightSchoolInfo.map(eachInfo => {
-    let el2 = htmlToElement(`<li style="color:#CA263D;">${eachInfo}</li>`);
+  let el4 = htmlToElement(`<strong><li style='color:#CA263D;'>Grades: ${childArray.GradeLevel}</li></strong>`);
+  HighLightSchool.appendChild(htmlToElement('<li style="color:#CA263D;"> <br></li>')).appendChild(el4);
+  HighLightSchoolInfo.forEach(eachInfo => {
+    let el2 = htmlToElement(`<li style='color:#CA263D;'>${eachInfo}</li>`);
     HighLightSchool.appendChild(el2);
-});
-  document.querySelector('body').style.gridTemplateAreas='"filters filters sorters nouse"  "map map list list2"';
-}
+  });
+  document.querySelector('body').style.gridTemplateAreas = '"filters filters sorters nouse"  "map map list list2"';
+};
 
 let showSchoolInfo = (school, marker) => {
   const dataUrl = `../../data/demographics/${school['ULCS Code']}.json`;
@@ -54,14 +54,14 @@ let showSchoolInfo = (school, marker) => {
     .then(data => {
       // let allGrades = data[-0];
       let allGrades = data
-        .filter( everyGrade => everyGrade['GradeLevel'] === "All Grades")[0];
+        .filter(everyGrade => everyGrade.GradeLevel === 'All Grades')[0];
       const sector = allGrades.Sector;
       const subSector = allGrades.SubSector;
       const grade = data
-        .filter( everyGrade => everyGrade['GradeLevel'] != "All Grades")
-        .map( everyGrade => everyGrade['GradeLevel']);
-      const AVGpctm = allGrades['MalePCT'];
-      const AVGpctf = allGrades['FemalePCT'];
+        .filter(everyGrade => everyGrade.GradeLevel !== 'All Grades')
+        .map(everyGrade => everyGrade.GradeLevel);
+      const AVGpctm = allGrades.MalePCT;
+      const AVGpctf = allGrades.FemalePCT;
 
       marker.bindPopup(`
         <h6>${school['Publication Name']}</h6>
@@ -74,10 +74,10 @@ let showSchoolInfo = (school, marker) => {
           <li>Average Percent Female: ${AVGpctf}%</li>
         </ul>`).openPopup();
 
-      HighLightSchool.innerHTML = "";
-      data.filter( everyGrade => everyGrade['GradeLevel'] != "All Grades").map( childArray => HighlightSchoolFunction(childArray));
-});
-}
+      HighLightSchool.innerHTML = '';
+      data.filter(everyGrade => everyGrade.GradeLevel !== 'All Grades').map(childArray => HighlightSchoolFunction(childArray));
+    });
+};
 
 /* PASTE YOUR WEEK4 EXERCISE CODE HERE */
 
@@ -87,11 +87,11 @@ let updateSchoolMarkers = (schoolsToShow) => {
   schoolsToShow.forEach((school) => {
     let [lat, lng] = school['GPS Location'].split(',');
     let stationName = school['Publication Name'];
-    let marker = L.marker([lat, lng]).bindTooltip(stationName)
+    let marker = L.marker([lat, lng]).bindTooltip(stationName);
     marker.addEventListener('click', () => {
       highlightLayer.clearLayers();
       showSchoolInfo(school, marker);
-      let highlightMarker = L.marker([lat, lng],{icon:redIcon});
+      let highlightMarker = L.marker([lat, lng], { icon: redIcon });
       highlightLayer.addLayer(highlightMarker);
     });
     schoolLayer.addLayer(marker);
@@ -99,19 +99,19 @@ let updateSchoolMarkers = (schoolsToShow) => {
 };
 
 let updateSchoolList = (schoolsToShow) => {
-  schoolList.innerHTML = "";
-  HighLightSchool.innerHTML = "";
-  let schoolList2 = schoolsToShow.map(school => school["Publication Name"]);
-  schoolList2.map(school => {
+  schoolList.innerHTML = '';
+  HighLightSchool.innerHTML = '';
+  let schoolList2 = schoolsToShow.map(school => school['Publication Name']);
+  schoolList2.forEach(school => {
     let el3 = htmlToElement(`<li>${school}</li>`);
     schoolList.appendChild(el3);
   });
 };
 
 let initializeZipCodeChoices = () => {
-  let zipCodeList = schools.map(school => school["Zip Code"].split('-')[0]);
+  let zipCodeList = schools.map(school => school['Zip Code'].split('-')[0]);
   let uniqueZipCodeList = [...new Set(zipCodeList)];
-  uniqueZipCodeList.map(uniqueZipCode => {
+  uniqueZipCodeList.forEach(uniqueZipCode => {
     let el = htmlToElement(`<option>${uniqueZipCode}</option>`);
     zipCodeSelect.appendChild(el);
   });
@@ -120,16 +120,14 @@ let initializeZipCodeChoices = () => {
 let filteredSchools = () => {
   let selectedGrade = gradeLevelSelect.options[gradeLevelSelect.selectedIndex].text;
   let selectedZipCode = zipCodeSelect.options[zipCodeSelect.selectedIndex].text;
-  if (selectedGrade === "All" && selectedZipCode === "All") {
+  if (selectedGrade === 'All' && selectedZipCode === 'All') {
     return schools;
-  } else if ( selectedGrade === "All" && selectedZipCode != "All" ) {
-      return schools.filter( school => school['Zip Code'].split('-')[0] === selectedZipCode);
-    } else if ( selectedGrade != "All" && selectedZipCode === "All" ) {
-      return schools.filter( school => school[`${selectedGrade}`] === '1' );
-    } else {
-      return schools.filter( school => school[`${selectedGrade}`] === '1' && school['Zip Code'].split('-')[0] === selectedZipCode);
-    };
-  }
+  } if (selectedGrade === 'All' && selectedZipCode !== 'All') {
+    return schools.filter(school => school['Zip Code'].split('-')[0] === selectedZipCode);
+  } if (selectedGrade !== 'All' && selectedZipCode === 'All') {
+    return schools.filter(school => school[`${selectedGrade}`] === '1');
+  } return schools.filter(school => school[`${selectedGrade}`] === '1' && school['Zip Code'].split('-')[0] === selectedZipCode);
+};
 
 /*
 
